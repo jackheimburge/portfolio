f11f9b8e1058bd6fd0d1e8b6888b5205d42572cc
centipedejam <jheimburge@gmail.com>
2023-10-10 20:16:06 -0700

    create welcome component, add background image and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar'
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Welcome />
     </div>
   );
 }
---
+++ src/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+    
+    return(
+        <div className="Welcome">
+            <h1>Hello</h1>
+        </div>
+    );
+}
