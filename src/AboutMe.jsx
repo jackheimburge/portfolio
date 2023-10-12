218122d3ca15ad2be109b76de8bdb33a3a188429
centipedejam <jheimburge@gmail.com>
2023-10-12 16:05:57 -0700

    add position/grid to welcome and about me

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -3,7 +3,15 @@ import './AboutMe.css';
 export default function AboutMe() {
     return(
         <div className="AboutMe">
-            <h2>ABOUT ME</h2>
+            <header>
+                <h2>A LITTLE ABOUT ME</h2>
+            </header>
+            <div className="statement">
+                Brand Statement
+            </div>
+            <div className="skills">
+                my skills
+            </div>
         </div>
     );
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,7 +2,7 @@ export default function Welcome() {
     
     return(
         <div className="Welcome">
-            <h1>Hello</h1>
+            <h1>Hello, I'm Jack heimburge.</h1>
         </div>
     );
 }
