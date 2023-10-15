aeadcfec27b76a3bfe0a10abf0bc7715b45a3ac2
centipedejam <jheimburge@gmail.com>
2023-10-15 12:03:05 -0700

    add basic styling to projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,7 +4,55 @@ import './Projects.css';
 export default function Projects() {
     return (
         <Element id="projects" className="Projects element">
-            <header>PROJECTS</header>
+            <div className="row">
+                <header>
+                    <h2>PROJECTS</h2>
+                </header>
+            </div>
+            <div className="skills-row row">
+                <div className="col-6">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="skills-row row">
+                <div className="col-6">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Element>
     );
 }
