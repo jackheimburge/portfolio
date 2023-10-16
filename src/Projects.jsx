55a84b4be473f3ca87e66f2895df98f94230bac8
centipedejam <jheimburge@gmail.com>
2023-10-15 17:13:35 -0700

    add photos and info to projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
                     <div className="card">
                         <div className="card-body">
                             <h5 className="card-title ">Tide Seeker</h5>
-                            <div id="carouselExample" class="carousel slide">
+                            <div id="tide-carousel" class="carousel slide">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
                                         <img src="https://i.imgur.com/8Uetvp9.png" class="d-block w-100" alt="..." />
@@ -26,11 +26,11 @@ export default function Projects() {
                                         <img src="https://i.imgur.com/Ui9357b.png" class="d-block w-100" alt="..." />
                                     </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                <button class="carousel-control-prev" type="button" data-bs-target="#tide-carousel" data-bs-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                <button class="carousel-control-next" type="button" data-bs-target="#tide-carousel" data-bs-slide="next">
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
@@ -45,11 +45,34 @@ export default function Projects() {
                 </div>
                 <div className="col-lg-6">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title ">DM Solutions</h5>
+                            <div id="dm-carousel" class="carousel slide">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://camo.githubusercontent.com/7a34cc37f60c949573fb5f352174f5d1b998e6fe4a39dc9fb471366a4fcd70fd/68747470733a2f2f692e696d6775722e636f6d2f6a6949654c76432e6a7067" class="d-block w-100" alt="..." />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://camo.githubusercontent.com/2506957c000d67ee51a684e00e3d4e734f1085b1c422ad8074870dfcd67c8b84/68747470733a2f2f692e696d6775722e636f6d2f613861305842792e706e6760" class="d-block w-100" alt="..." />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://camo.githubusercontent.com/c80fbc5e73a645413b235eaedcab4683016664e7c8905a9af977c7ad76039206/68747470733a2f2f692e696d6775722e636f6d2f445767596851702e6a7067" class="d-block w-100" alt="..." />
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#dm-carousel" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#dm-carousel" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <p className="card-text mt-4">- Dealership solution to help track sales and inventory</p>
+                            <p className="card-text mt-4">- Uses chart insights to visualize sales data.  </p>
+                            <p className="card-text mt-4">- Built using Python, Django and Chart.js</p>
+                            <a href="https://tide-seeker-1959079ff47b.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">Live</a>
+                            <a href="https://github.com/jackheimburge/tide-seeker" target="_blank" className="btn btn-primary projects-btn">Github</a>
                         </div>
                     </div>
                 </div>
@@ -57,21 +80,64 @@ export default function Projects() {
             <div className="skills-row row">
                 <div className="col-lg-6">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title ">Blackjack</h5>
+                            <div id="blackjack-carousel" class="carousel slide">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://i.imgur.com/XjfWYSu.png" class="d-block w-100" alt="..." />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://i.imgur.com/XjfWYSu.png" class="d-block w-100" alt="..." />
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#blackjack-carousel" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#blackjack-carousel" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <p className="card-text mt-4">- Seemlisly view your favorite surf spots and locations.</p>
+                            <p className="card-text mt-4">- Track your surf sessions, including photo upload and conditions.</p>
+                            <p className="card-text mt-4">- Built using Node.js and Express.js</p>
+                            <a href="https://tide-seeker-1959079ff47b.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">Live</a>
+                            <a href="https://github.com/jackheimburge/tide-seeker" target="_blank" className="btn btn-primary projects-btn">Github</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-6">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title ">Card Hub</h5>
+                            <div id="card-hub-carousel" class="carousel slide">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://i.imgur.com/8Uetvp9.png" class="d-block w-100" alt="..." />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://i.imgur.com/Wi4hXau.png" class="d-block w-100" alt="..." />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://i.imgur.com/Ui9357b.png" class="d-block w-100" alt="..." />
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#card-hub-carousel" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#card-hub-carousel" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <p className="card-text mt-4">- Seemlisly view your favorite surf spots and locations.</p>
+                            <p className="card-text mt-4">- Track your surf sessions, including photo upload and conditions.</p>
+                            <p className="card-text mt-4">- Built using Node.js and Express.js</p>
+                            <a href="https://tide-seeker-1959079ff47b.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">Live</a>
+                            <a href="https://github.com/jackheimburge/tide-seeker" target="_blank" className="btn btn-primary projects-btn">Github</a>
                         </div>
                     </div>
                 </div>
