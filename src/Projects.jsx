fe7666ba2415be567f0ec45d082e2322c0bf8db9
centipedejam <jheimburge@gmail.com>
2023-11-02 10:29:30 -0700

    update project links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -39,7 +39,7 @@ export default function Projects() {
                             <p className="card-text mt-4">- Track your surf sessions, including photo upload and conditions.</p>
                             <p className="card-text mt-4">- Built using Node.js and Express.js</p>
                             <a href="https://tide-seeker-1959079ff47b.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">View Site</a>
-                            <a href="https://github.com/jackheimburge/tide-seeker" target="_blank" className="btn btn-primary projects-btn">Github</a>
+                            <a href="https://github.com/jackheimburge/tide-seeker" target="_blank" className="btn btn-primary projects-btn">GitHub</a>
                         </div>
                     </div>
                 </div>
@@ -72,7 +72,7 @@ export default function Projects() {
                             <p className="card-text mt-4">- Uses chart insights to visualize sales data.  </p>
                             <p className="card-text mt-4">- Built using Python, Django and Chart.js</p>
                             <a href="https://dm-solutions-6a34df6ef1ad.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">View Site</a>
-                            <a href="https://github.com/jackheimburge/dm-solutions" target="_blank" className="btn btn-primary projects-btn">Github</a>
+                            <a href="https://github.com/jackheimburge/dm-solutions" target="_blank" className="btn btn-primary projects-btn">GitHub</a>
                         </div>
                     </div>
                 </div>
@@ -85,10 +85,10 @@ export default function Projects() {
                             <div id="blackjack-carousel" class="carousel slide">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="https://i.imgur.com/4lYbtap.png" class="d-block w-100" alt="..." />
+                                        <img src="https://i.imgur.com/WI8O9IS.png" class="d-block w-100" alt="..." />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="https://i.imgur.com/WI8O9IS.png" class="d-block w-100" alt="..." />
+                                        <img src="https://i.imgur.com/4lYbtap.png" class="d-block w-100" alt="..." />
                                     </div>
                                     <div class="carousel-item">
                                         <img src="https://i.imgur.com/0p0mCqx.png" class="d-block w-100" alt="..." />
@@ -110,7 +110,7 @@ export default function Projects() {
                             <p className="card-text mt-4">- Crafted an engaging shopping experience with a modern user interface layout.</p>
                             <p className="card-text mt-4">- Leveraged the power of the MERN stack. </p>
                             <a href="https://card-hub-fc0733ce387e.herokuapp.com/" target="_blank" className="btn btn-primary projects-btn">View Site</a>
-                            <a href="https://jackheimburge.github.io/card-hub/" target="_blank" className="btn btn-primary projects-btn">Git Hub</a>
+                            <a href="https://github.com/jackheimburge/card-hub" target="_blank" className="btn btn-primary projects-btn">GitHub</a>
                         </div>
                     </div>
                 </div>
@@ -143,7 +143,7 @@ export default function Projects() {
                             <p className="card-text mt-4">- A fun user vs computer game played just like at the casino!</p>
                             <p className="card-text mt-4">- Features a modern and sleak user interface!</p>
                             <a href="https://jackheimburge.github.io/blackjack/" target="_blank" className="btn btn-primary projects-btn">View Site</a>
-                            <a href="https://github.com/jackheimburge/blackjack" target="_blank" className="btn btn-primary projects-btn">Github</a>
+                            <a href="https://github.com/jackheimburge/blackjack" target="_blank" className="btn btn-primary projects-btn">GitHub</a>
                         </div>
                     </div>
                 </div>
